c069895d721e604d087764d46d7c904c6a837743
dlearnerr <dlearner1623@gmail.com>
2026-04-08 23:37:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ class H(SimpleHTTPRequestHandler):
         s.end_headers()
         s.wfile.write(b"CI/CD Success")
 
-HTTPServer(("",5020),H).serve_forever()
+HTTPServer(("",5022),H).serve_forever()
